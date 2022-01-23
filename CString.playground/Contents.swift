7257cb269f6ca8d7cd658a6abd9a1e73df48be75
BaeChangBum <changbum10001@gmail.com>
2022-01-23 23:55:11 +0900

    Added Cstring function

--- CString.playground/Contents.swift
+++ CString.playground/Contents.swift
@@ -18,3 +18,27 @@ print("I like bitcoin and cheeseCoin")
 print("I like \(bitCoin) and \(cheeseCoin)")
 print("I boutht \(1) \(cheeseCoin) for $\(1 * 1000)")
 
+for char in cheeseCoin{
+    print("char: \(char)")
+}
+
+print(cheeseCoin.count)
+print(cheeseCoin.lowercased())
+print(cheeseCoin.first)
+print("".first)
+print(bitCoin.removeFirst())    //removes and returns the first Element
+print(bitCoin.replacingOccurrences(of: "it", with: "Ghost"))
+print(bitCoin.sorted())
+print(bitCoin.split(separator: "t"))
+
+bitCoin.append(" is interesting.")
+print(bitCoin)
+
+if(bitCoin == cheeseCoin)
+{
+    print("I'm not sure why these are the same")
+}
+else
+{
+    print("Good.")
+}
