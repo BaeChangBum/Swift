37b55bac4342c6499d8f73473011931bfb9c6357
BaeChangBum <changbum10001@gmail.com>
2022-01-23 23:42:52 +0900

    Added print function

--- CString.playground/Contents.swift
+++ CString.playground/Contents.swift
@@ -2,3 +2,19 @@ import UIKit
 
 var bitCoin = "Bitcoin"
 let cheeseCoin = "CheeseCoin"
+
+var long = """
+    this string is
+    far too long to
+only fit on one line;
+"""
+
+print(bitCoin)
+print(cheeseCoin)
+print(long)
+print(bitCoin + cheeseCoin)
+print(bitCoin + " " + cheeseCoin)
+print("I like bitcoin and cheeseCoin")
+print("I like \(bitCoin) and \(cheeseCoin)")
+print("I boutht \(1) \(cheeseCoin) for $\(1 * 1000)")
+
