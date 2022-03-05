0d47e0b08232c5782bd0a7b7ee49f2cae3de9cb8
BaeChangBum <changbum10001@gmail.com>
2022-03-05 18:59:53 +0900

    연산 프로퍼티, 타입 프로퍼티

--- Study/문법/프로퍼티와 메서드.playground/Contents.swift
+++ Study/문법/프로퍼티와 메서드.playground/Contents.swift
@@ -57,7 +57,7 @@ print("changbum's Point : \(changbumPosition.point?.x), \(changbumPosition.point
 
 
 // 지연저장 프로퍼티
-struct CoordinatePoint{
+/*struct CoordinatePoint{
     var x: Int = 0
     var y: Int = 0
 }
@@ -80,6 +80,164 @@ let changbumPosition: Position = Position(name: "changbum")
 // point 프로퍼티의 CoordinatePoint가 생성된다.
 // 다중 스레드 환경에서는 스레드가 비슷한 시점에 접근한다면, 여러번 초기화될 수 있다.
 print("changbum's Point : \(changbumPosition.point)")
-
+*/
 
 // 연산 프로퍼티
+struct CoordinatePoint{
+    var x: Int
+    var y: Int
+
+    var oppositePoint: CoordinatePoint{
+        get{
+            return CoordinatePoint(x: -x, y: -y)
+        }
+        
+        set(opposite){
+            x = -opposite.x
+            y = -opposite.y
+        }
+    }
+    /*// 대칭점을 구하는 메서드 - 접근자
+    // self 대신 CoordinatePoint를 사용해도 된다.
+    func oppositePoint() -> Self{
+        return CoordinatePoint(x: -x, y : -y)
+    }
+    
+    // 대칭점을 설정하는 메서드 - 설정자
+    mutating func setOppositePoint(_ opposite: CoordinatePoint) {
+        x = -opposite.x
+        y = -opposite.y
+    }*/
+}
+
+var changbumPosition: CoordinatePoint = CoordinatePoint(x: 10, y: 20)
+
+// 현재 좌표
+print(changbumPosition)
+
+// 대칭 좌표
+print(changbumPosition.oppositePoint)
+
+changbumPosition.oppositePoint = CoordinatePoint(x: 15, y: 30)
+
+print(changbumPosition)
+
+// 상속받은 연산 프로퍼티의 프로퍼티 감시자 구현
+/*class Account{
+    var credit: Int = 0{
+        willSet(newValue) {
+            print("잔액이 \(credit)원에서 \(newValue)원으로 변경될 예정입니다.")
+        }
+    
+        didSet(oldValue) {
+            print("잔액이 \(oldValue)원에서 \(credit)원으로 변경되었습니다.")
+    
+        }
+    }
+    
+    var dollarValue: Double{
+        get{
+            return Double(credit)
+        }
+        
+        set{
+            credit = Int(newValue * 1000)
+            print("잔액을 \(newValue)달러로 변경 중 입니다.")
+        }
+    }
+}
+
+class ForeignAccount: Account{
+    override var dollarValue: Double{
+        willSet{
+            print("잔액이 \(dollarValue)달러에서 \(newValue)달러로 변경 될 예정입니다.")
+        }
+        
+        didSet{
+            print("잔액이 \(oldValue)달러에서 \(dollarValue)달러로 변경되었습니다.")
+        }
+    }
+}
+
+let myAccount: ForeignAccount = ForeignAccount()
+myAccount.credit = 1000
+
+myAccount.dollarValue = 2
+*/
+
+// 저장변수의 감시자와 연산 변수
+var workInPocket: Int = 2000{
+    willSet{
+        print("주머니의 돈이 \(workInPocket)원에서 \(newValue)원으로 변경될 예정입니다.")
+    }
+    didSet{
+        print("주머니의 돈이 \(oldValue)원에서 \(workInPocket)원으로 변경되었습니다.")
+    }
+}
+
+var dollarInPocket: Double{
+    get{
+        return Double(workInPocket) / 1000.0
+    }
+    set{
+        workInPocket = Int(newValue * 1000.0)
+        print("주머니의 달러를 \(newValue)달러로 변경 중입니다.")
+    }
+}
+
+dollarInPocket = 3.5
+
+
+// 타입 프로퍼티와 인스턴스 프로퍼티
+class AClass{
+    // 저장 타입 프로퍼티
+    static var typeProperty: Int = 0
+    
+    // 저장 인스턴스 프로퍼티
+    var instanceProperty: Int = 0{
+        didSet{
+            Self.typeProperty = instanceProperty + 100
+        }
+    }
+    
+    // 연산 타입 프로퍼티
+    static var typeComputedProperty: Int{
+        get{
+            return typeProperty
+        }
+        set{
+            typeProperty = newValue
+        }
+    }
+}
+
+AClass.typeProperty = 123
+
+let classInstace: AClass = AClass()
+classInstace.instanceProperty = 100
+
+print(AClass.typeProperty)
+print(AClass.typeComputedProperty)
+
+// 타입 프로퍼티의 사용
+class Account{
+    static let dollarExchangeRate: Double = 1000.0
+    
+    var credit: Int = 0
+    
+    var dollarValue: Double{
+        get{
+            return Double(credit)
+        }
+        set{
+            credit = Int(newValue * Account.dollarExchangeRate)
+            print("잔액을 \(newValue)달러로 변경 중입니다.")
+            print(credit)
+        }
+    }
+}
+
+var value:Account = Account()
+value.dollarValue = 2000.0
+
+
