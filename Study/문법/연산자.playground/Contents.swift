36afc0e9f98950bc41774511a0c923f5036a5ecf
BaeChangBum <changbum10001@gmail.com>
2022-03-30 23:37:36 +0900

    중위연산자

--- Study/문법/연산자.playground/Contents.swift
+++ Study/문법/연산자.playground/Contents.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Foundation
 
 // 삼항 연산자
 var valueA:Int = 3
@@ -28,5 +29,91 @@ var unsignedInterger: UInt8 = 0
 let underflowdValue: UInt8 = unsignedInterger &- 1          // 255
 
 unsignedInterger = UInt8.max                                // 255
-let errorOverflowResult: UInt8 = unsignedInterger + 1       // Error
+//let errorOverflowResult: UInt8 = unsignedInterger + 1       // Error
 let overflowValue: UInt8 = unsignedInterger &+ 1            // 0
+
+
+// 전위 연산자
+/*prefix operator **
+// prefix를 func 키워드 앞에 사용하여 아래 연산자를 사용하겠다는 의미
+prefix func ** (value: Int) -> Int {
+    return value * value
+}
+
+let minsuFive: Int = -5
+let sqrtMinsuFive: Int = **minsuFive
+
+print(sqrtMinsuFive)*/
+
+prefix func ! (value: String) -> Bool {
+    return value.isEmpty
+}
+
+var stringValue: String = "Changbum"
+var isEmptyString: Bool = !stringValue
+
+print(isEmptyString)
+
+stringValue = ""
+isEmptyString = !stringValue
+
+print(isEmptyString)
+
+prefix operator **
+
+prefix func ** (value: String) -> String {
+    return value + " " + value
+}
+
+let resultString: String = **"changbum"
+print(resultString)
+
+
+postfix operator **
+postfix func ** (value: Int) -> Int {
+    return value + 10
+}
+
+infix operator ** : MultiplicationPrecedence
+
+func ** (lhs: String, rhs: String) -> Bool {
+    return lhs.contains(rhs)
+}
+
+let helloChangbum: String = "Hello Changbum"
+let changbum: String = "Changbum"
+let isContainsChangbum: Bool = helloChangbum ** changbum
+
+// 클래스 및 구조체의 비교 연산자
+class car {
+    var modelYear: Int?
+    var modelName: String?
+}
+
+struct SmartPhone {
+    var company: String?
+    var model: String?
+}
+
+func == (lhs: car, rhs: car) -> Bool {
+    return lhs.modelName == rhs.modelName
+}
+
+func == (lhs: SmartPhone, rhs: SmartPhone) -> Bool {
+    return lhs.model == rhs.model
+}
+
+let myCar = car()
+myCar.modelName = "S"
+
+let yourCar = car()
+yourCar.modelName = "S"
+
+var myPhone = SmartPhone()
+myPhone.model = "SE"
+
+var yourPhone = SmartPhone()
+yourPhone.model = "6"
+
+print(myCar == yourCar)
+print(myPhone == yourPhone)
